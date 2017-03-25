796da666c176c577f056e5e17504b4d4a2696aad
曾传文 <zcw@howyou.com>
2017-03-25 22:36:10 +0800

    Initial Commit

---
+++ GitPushDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitPushDemo
+//
+//  Created by 曾传文 on 2017/3/25.
+//  Copyright © 2017年 hero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitPushDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitPushDemo
+//
+//  Created by 曾传文 on 2017/3/25.
+//  Copyright © 2017年 hero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
